ad614b3db062e73ae868c5e567b41204ca651cdc
ania <121018874+pozniej-znajde-wolne-haslo@users.noreply.github.com>
2023-09-26 16:23:06 +0200

    Update App.jsx

--- Expenses-APP/src/App.jsx
+++ Expenses-APP/src/App.jsx
@@ -1,19 +1,1 @@
 
-import Income from "./components/Income/Income";
-import ContextProvider from "./contexts/ContextProvider";
-import { MainContext } from "./contexts/mainContext";
-import './App.css';
-
-function App() {
-  return( 
-  
-<ContextProvider>
-
-<Income/>
-
-</ContextProvider>
-
-  );
-}
-
-export default App;
