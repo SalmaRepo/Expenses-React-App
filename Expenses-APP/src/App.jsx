e6b2eee63931649c0a5da0961bcd173b1062943b
Salma <salma.shaikh751@gmail.com>
2023-09-26 16:00:34 +0200

    60% of functional part is done, might need to remove some components, and struggling with reload getting data

--- Expenses-APP/src/App.jsx
+++ Expenses-APP/src/App.jsx
@@ -1,5 +1,37 @@
+import Income from "./components/Income/Income";
+
+
+import './App.css';
+import IncomeContextProvider from "./contexts/IncomeContext/IncomeContextProvider";
+import SpendingContextProvider from "./contexts/SpendingContext/SpendingContextProvider";
+import Spending from "./components/Spending/Spending";
+import TodayBalance from "./components/TodayBalance/TodayBalance";
+import History from "./components/History/History";
+
 function App() {
-  return <></>;
+  return( 
+    <div style={{width:'50%',margin:'0 auto',backgroundColor:'lightYellow',border:'2px solid black',padding:'2rem'}}>
+<IncomeContextProvider>
+
+
+<SpendingContextProvider>
+
+
+<TodayBalance/>
+<Income/>
+  <Spending/>
+ 
+ <History/>
+  
+</SpendingContextProvider>
+
+</IncomeContextProvider>  
+
+
+
+
+</div>
+  );
 }
 
 export default App;
